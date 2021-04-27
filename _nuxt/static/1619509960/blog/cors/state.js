594ee5b662ad1b28993a7084fc2c8e3aa5562b4c
chris-cmsoft <chris-cmsoft@users.noreply.github.com>
2021-04-27 08:12:41 +0000

    deploy: 348ebd4caeb6343725d16e9bcf488f01497f8567

--- _nuxt/static/1619509960/blog/cors/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619509960",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fcors",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"de53f6c9"}}}}(null));
