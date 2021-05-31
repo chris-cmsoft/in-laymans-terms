14e13bc5384447c2a4711206a9c900f8e08f791b
chris-cmsoft <chris-cmsoft@users.noreply.github.com>
2021-05-31 15:11:50 +0000

    deploy: d38dba023d1b2e3726ffbc6e30bbc4e83e7d93ef

--- _nuxt/static/1619791345/blog/category/web/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619791345",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fcategory\u002Fweb",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"40990d79"}}}}(null));
