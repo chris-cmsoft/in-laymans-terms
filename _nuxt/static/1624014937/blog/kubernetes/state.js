c3c1efd11694e187b3a8f509ae774a0981726a13
chris-cmsoft <chris-cmsoft@users.noreply.github.com>
2021-06-18 11:18:46 +0000

    deploy: bed1ee5d71149534bca7dbbb21a49d5cb120d53b

--- _nuxt/static/1624014937/blog/kubernetes/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624014937",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fkubernetes",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cbeec374"}}}}(null));
