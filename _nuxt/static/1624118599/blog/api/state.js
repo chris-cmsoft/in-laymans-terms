241ab360964c2aa8d00f97087b84d142bf94ebdf
chris-cmsoft <chris-cmsoft@users.noreply.github.com>
2021-06-19 16:05:48 +0000

    deploy: a58973b4fb8b0646de39df68439840b11fdd62be

--- _nuxt/static/1624118599/blog/api/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624118599",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fapi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"faea450d"}}}}(null));
