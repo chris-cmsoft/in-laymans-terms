f0677cd3cc4b53f7bb8b41cb6527e45093d6dd7f
chris-cmsoft <chris-cmsoft@users.noreply.github.com>
2021-07-16 13:59:49 +0000

    deploy: 2cf0b6599e6f331f4f7e5167258b64756e5e210e

---
+++ _nuxt/static/1626443988/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog\u002Fapi","\u002Fblog\u002Fcdn","\u002Fblog\u002Fcors","\u002Fblog\u002Fdomain","\u002Fblog\u002Fedge-computing","\u002Fblog\u002Fgrep","\u002Fblog\u002Fkubernetes","\u002Fblog\u002Fplatform","\u002Fblog\u002Fcontainer","\u002Fblog\u002Fauthor\u002Fchris"]})
---
+++ _nuxt/static/1626443988/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626443988",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"119b8c0c"}}}}(null,"\u002F"));
