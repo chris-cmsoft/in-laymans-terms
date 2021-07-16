54769afdc6f0a5a35c49373364523155eec84286
chris-cmsoft <chris-cmsoft@users.noreply.github.com>
2021-07-16 12:19:59 +0000

    deploy: 5691f404b30047742673d02a0d2622904aba79ea

---
+++ _nuxt/static/1626437998/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626437998",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62615b05"}}}}(null,"\u002F"));
