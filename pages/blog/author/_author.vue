a1c9c31d89c07e09cc7421964fa97425836dd2c8
Chris Vermeulen <chris@linguine.tech>
2021-07-16 17:14:07 +0200

    Linter fix errors (#60)
    * Add initial edge computing article
    
    * More friendly language, and typo
    
    * Upgrade all packages
    
    * Downgrade sass loader as new version not compatible with Nuxt
    
    * Run generate with CI to ensure transpiler works correctly
    
    * Update test to only pass for truthy
    
    * Fix linting issues

--- pages/blog/author/_author.vue
+++ pages/blog/author/_author.vue
@@ -36,7 +36,7 @@
     <div class="max-w-6xl mx-auto px-5 py-24 ">
       <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
         <div class="w-36 mb-8">
-          <img :src="author.avatarUrl" class="rounded-full shadow-inner"/>
+          <img :src="author.avatarUrl" class="rounded-full shadow-inner">
         </div>
         <h1 class=" title-font mb-2 text-4xl font-extrabold leading-10 tracking-tight text-left sm:text-5xl sm:leading-none md:text-6xl">
           {{ author.name }}
@@ -47,23 +47,23 @@
       </div>
       <div class="flex flex-wrap justify-center -m-4">
         <div v-for="article of articles" :key="article.slug" class="xl:w-1/3 md:w-1/2 p-4">
-          <article-listing :article="article"></article-listing>
+          <article-listing :article="article" />
         </div>
       </div>
     </div>
-<!--    <p>Bio: {{ articles[0].author.bio }}</p>-->
-<!--    <h3>Here are a list of articles by {{ articles[0].author.name }}:</h3>-->
-<!--    <ul>-->
-<!--      <li v-for="article in articles" :key="article.slug">-->
-<!--        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">-->
-<!--          <img :src="article.img" :alt="article.alt">-->
-<!--          <div>-->
-<!--            <h2>{{ article.title }}</h2>-->
-<!--            <p>{{ article.description }}</p>-->
-<!--            <p>{{ formatDate(article.updatedAt) }}</p>-->
-<!--          </div>-->
-<!--        </NuxtLink>-->
-<!--      </li>-->
-<!--    </ul>-->
+    <!--    <p>Bio: {{ articles[0].author.bio }}</p>-->
+    <!--    <h3>Here are a list of articles by {{ articles[0].author.name }}:</h3>-->
+    <!--    <ul>-->
+    <!--      <li v-for="article in articles" :key="article.slug">-->
+    <!--        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">-->
+    <!--          <img :src="article.img" :alt="article.alt">-->
+    <!--          <div>-->
+    <!--            <h2>{{ article.title }}</h2>-->
+    <!--            <p>{{ article.description }}</p>-->
+    <!--            <p>{{ formatDate(article.updatedAt) }}</p>-->
+    <!--          </div>-->
+    <!--        </NuxtLink>-->
+    <!--      </li>-->
+    <!--    </ul>-->
   </div>
 </template>
