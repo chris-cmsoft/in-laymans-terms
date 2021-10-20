d1d6aa1fc4e2b20d980b4dd24d4dc4cc20ca64b3
Charlotte Mach <52953502+charlottemach@users.noreply.github.com>
2021-10-20 15:37:27 +0200

    Fix typo in Title (#149)

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'inlamensterms.io',
+    title: 'inlaymansterms.io',
     htmlAttrs: {
       lang: 'en'
     },
