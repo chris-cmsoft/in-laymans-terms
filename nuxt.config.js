59fd25a4854e81677708436a9653c835e048a555
Charlotte Mach <52953502+charlottemach@users.noreply.github.com>
2021-10-20 12:06:02 +0200

    Fix type

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'inlamensterms.io',
+    title: 'inlaymensterms.io',
     htmlAttrs: {
       lang: 'en'
     },
